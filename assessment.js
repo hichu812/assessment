3e053f22553125a6d6d85f47ec543a7f5abbce6c
hichu812 <67946960+hichu812@users.noreply.github.com>
2020-07-07 16:51:09 +0900

    名前に「さん」をつけました

--- assessment.js
+++ assessment.js
@@ -93,7 +93,7 @@ function assessment(userName) {
     const index = sumOfCharCode % answers.length;
     let result = answers[index];
 
-    result = result.replace(/\{userName\}/g, userName);
+    result = result.replace(/\{userName\}/g, userName + 'さん');
     return result;
 }
 
